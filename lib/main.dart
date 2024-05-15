27c1db2fa261d43cfcd738d55d075181cfd5673f
germandbl <gdb.lopez@outlook.com>
2024-05-15 17:28:22 -0600

    Fix conflict

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:laboratorio2_app/screens/podcast_screen.dart';
+import 'screens/task_list_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PodcastScreen(),
+      home: TaskListScreen(),
     );
   }
 }
