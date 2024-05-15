209e2d08b25a343f251a3f49ba2c81facfc66d83
germandbl <gdb.lopez@outlook.com>
2024-05-15 17:32:51 -0600

    Refactorizacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/task_list_screen.dart';
+import 'package:laboratorio2_app/screens/podcast_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TaskListScreen(),
+      home: PodcastScreen(),
     );
   }
 }
