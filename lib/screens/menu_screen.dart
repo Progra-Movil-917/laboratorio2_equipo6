11d00dd9313e38c1530b14dec4a2d38d64f89bbf
germandbl <gdb.lopez@outlook.com>
2024-05-15 22:12:32 -0600

    Creacion del menu inicial

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -5,6 +5,54 @@ class MenuScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
+        title: const Text('App CEUTEC'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            contentPadding: const EdgeInsets.all(5),
+            title: const Text('Ir a la sección de noticias'),
+            trailing: const Icon(
+              Icons.arrow_forward,
+              size: 25,
+            ),
+            onTap: () {},
+          ),
+          const Divider(),
+          ListTile(
+            contentPadding: const EdgeInsets.all(5),
+            title: const Text('Ir a la lista de tareas'),
+            trailing: const Icon(
+              Icons.arrow_forward,
+              size: 25,
+            ),
+            onTap: () {},
+          ),
+          const Divider(),
+          ListTile(
+            contentPadding: const EdgeInsets.all(5),
+            title: const Text('Ir al conversor de moneda'),
+            trailing: const Icon(
+              Icons.arrow_forward,
+              size: 25,
+            ),
+            onTap: () {},
+          ),
+          const Divider(),
+          ListTile(
+            contentPadding: const EdgeInsets.all(5),
+            title: const Text('Ir al podcast'),
+            trailing: const Icon(
+              Icons.arrow_forward,
+              size: 25,
+            ),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
